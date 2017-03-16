012b270b18c7c6c468c475aec296cf5001c70173
ramez aissaoui <ramez aissaoui>
2017-03-16 08:50:46 +0100

    scraping data from Promo vacances

---
+++ Scraping_Data_From_PromoVoyage.js
@@ -0,0 +1,83 @@
+/*var express = require('express');
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+function clean(text){
+  text.replace(/(\r\n|\n|\r)/gm,"");
+  text=text.trim();
+  return text;
+}
+
+var pays="espagne";
+var prices;
+var url="http://www.promovacances.com/vacances-sejour-hotel/voyage-"+pays+"/#destinationZones=1761&departureCities=1188&themespace=sejour-voyage&departureDateRange=15&moteur=true";
+
+
+*/
+
+/*var PromoVacances_price = function(url,callback){
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+        $('#coup-de-coeur-list > div:nth-child(1) > div > div.info > div.bloc-prix > div.prix > a').filter(function(){
+          var data=$(this).text();
+
+          price =data;
+
+          console.log(price);
+        })
+    }
+    callback(price);
+  })
+}
+
+exports.PromoVacances_price = PromoVacances_price*/
+
+
+
+
+
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+
+
+app.get('/scrape', function(req, res){
+  var pays="espagne";
+  var prices;
+  var url="http://www.promovacances.com/vacances-sejour-hotel/voyage-espagne/#destinationZones=1761&departureCities=1188&themespace=sejour-voyage&departureDateRange=15&moteur=true";
+
+request(url, function(error, response, html){
+    if(!error){
+        var $ = cheerio.load(html);
+
+        var json = { price: ""};
+
+    $('#coup-de-coeur-list > div:nth-child(1) > div > div.info > div.bloc-prix > div.prix > a > p:nth-child(2)').filter(function(){
+        var data=$(this).text();
+        price =data;
+
+        json.price = price;
+
+    })
+}
+
+fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+
+    console.log('File successfully written! - Check your project directory for the output.json file');
+
+})
+
+res.send('Check your console!')
+
+    }) ;
+})
+
+app.listen('3000');
+
+console.log('Magic happens on port 3000');
