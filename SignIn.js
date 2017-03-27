571246a2cf1b167634e6936545a891465fa60c54
aissaoui <ramez.aissaoui@devinci.fr>
2017-03-27 13:06:10 +0200

    Home

---
+++ SignIn.js
@@ -0,0 +1,24 @@
+var player = document.querySelector('#audioPlayer');
+player.play();
+function play(idPlayer, control) {
+    var player = document.querySelector('#' + idPlayer);
+
+    if (player.paused) {
+        player.play();
+        control.textContent = 'Pause';
+    } else {
+        player.pause();
+        control.textContent = 'Play';
+    }
+}
+
+function resume(idPlayer) {
+    var player = document.querySelector('#' + idPlayer);
+
+    player.currentTime = 0;
+    player.pause();
+}
+player.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
